ef5f7535eb0d310f23a4d90667cf19b3db8f741a
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-08-19 16:58:27 -0400

    added Food to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Food } from "./Food.jsx";
 import Greeting from "./Greeting.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Greeting />
+    <Food />
   </React.StrictMode>
 );
