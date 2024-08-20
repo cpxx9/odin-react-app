b676287dadddfa24ffebc4d1468de645aa7e0f23
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-08-20 07:45:20 -0400

    finished top lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import "./App.css";
 
+function List(props) {
+  if (!props.animals) {
+    return <div>Loading...</div>;
+  }
+
+  if (props.animals.length === 0) {
+    return <div>There are no animals in the list!</div>;
+  }
+
+  return (
+    <ul>
+      {props.animals.map((animal) => {
+        return <li key={animal}>{animal}</li>;
+      })}
+    </ul>
+  );
+}
+
 function App() {
-  const animals = ["Lion", "Cow", "Snake", "Lizard"];
+  const animals = [];
 
   return (
     <div>
       <h1>Animals: </h1>
-      <ul>
-        {animals.map((animal) => {
-          return <li key={animal}>{animal}</li>;
-        })}
-      </ul>
+      <List animals={animals} />
     </div>
   );
 }
