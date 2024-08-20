fb4a243ef82ba14d30e824ac64ffd9b83b49eab2
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-08-20 12:49:35 -0400

    uuid pkg

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,60 @@
 import './App.css';
 
-function Item(props) {
-  let itemContent = props.name;
-  if (props.isPacked) {
-    itemContent = <del>{props.name + ' ✔'}</del>;
-  }
+export const people = [
+  {
+    id: 0,
+    name: 'Creola Katherine Johnson',
+    profession: 'mathematician',
+    accomplishment: 'spaceflight calculations',
+    imageId: 'MK3eW3A',
+  },
+  {
+    id: 1,
+    name: 'Mario José Molina-Pasquel Henríquez',
+    profession: 'chemist',
+    accomplishment: 'discovery of Arctic ozone hole',
+    imageId: 'mynHUSa',
+  },
+  {
+    id: 2,
+    name: 'Mohammad Abdus Salam',
+    profession: 'physicist',
+    accomplishment: 'electromagnetism theory',
+    imageId: 'bE7W1ji',
+  },
+  {
+    id: 3,
+    name: 'Percy Lavon Julian',
+    profession: 'chemist',
+    accomplishment:
+      'pioneering cortisone drugs, steroids and birth control pills',
+    imageId: 'IOjWm71',
+  },
+  {
+    id: 4,
+    name: 'Subrahmanyan Chandrasekhar',
+    profession: 'astrophysicist',
+    accomplishment: 'white dwarf star mass calculations',
+    imageId: 'lrWQx8l',
+  },
+];
 
-  return <li className="item">{itemContent}</li>;
+function getImageUrl(person) {
+  return 'https://i.imgur.com/' + person.imageId + 's.jpg';
 }
 
 function App() {
-  return (
-    <section>
-      <h1>Sally Ride&apos;s Packing List</h1>
-      <ul>
-        <Item isPacked={true} name="Space suit" />
-        <Item isPacked={true} name="Helmet with gold tie" />
-        <Item isPacked={false} name="Photo of Tam" />
-      </ul>
-    </section>
-  );
+  const listItems = people.map((person) => (
+    <li key={person.id}>
+      <img src={getImageUrl(person)} alt={person.name} />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  ));
+  return <ul>{listItems}</ul>;
 }
 
 export default App;
