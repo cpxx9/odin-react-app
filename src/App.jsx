86415bfaa4ae5fe444fa36a7b3387424bd5b70eb
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-08-21 18:00:51 -0400

    color changer example to work on

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
 import './App.css';
 // import { v4 as uuid } from 'uuid';
-import { sculptureList } from './data';
 import { useState } from 'react';
 
+const COLORS = ['pink', 'green', 'blue', 'yellow', 'purple'];
+
 function App() {
-  const [index, setIndex] = useState(0);
-  const [showMore, setShowMore] = useState(false);
+  const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
 
-  function handleNextClick() {
-    setIndex(index + 1);
-  }
+  const onButtonClick = (color) => () => {
+    setBackgroundColor(color);
+  };
 
-  function handleMoreClick() {
-    setShowMore(!showMore);
-  }
-
-  let sculpture = sculptureList[index];
   return (
-    <>
-      <button onClick={handleNextClick}>Next</button>
-      <h2>
-        <i>{sculpture.name}</i> by {sculpture.artist}
-      </h2>
-      <h3>
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <button onClick={handleMoreClick}>{showMore ? 'Hide' : 'Show'}</button>
-      {showMore && <p>{sculpture.description}</p>}
-      <img src={sculpture.url} alt={sculpture.alt} />
-    </>
+    <div
+      className="App"
+      style={{
+        backgroundColor,
+      }}
+    >
+      {COLORS.map((color) => (
+        <button
+          type="button"
+          key={color}
+          onClick={onButtonClick(color)}
+          className={backgroundColor === color ? 'selected' : ''}
+        >
+          {color}
+        </button>
+      ))}
+    </div>
   );
 }
 
