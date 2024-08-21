7cf2a576650742baa34128b6fb29938eda189eb7
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-08-20 21:29:03 -0400

    props lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,20 @@
 import './App.css';
+import { v4 as uuid } from 'uuid';
 
-export const people = [
-  {
-    id: 0,
-    name: 'Creola Katherine Johnson',
-    profession: 'mathematician',
-    accomplishment: 'spaceflight calculations',
-    imageId: 'MK3eW3A',
-  },
-  {
-    id: 1,
-    name: 'Mario José Molina-Pasquel Henríquez',
-    profession: 'chemist',
-    accomplishment: 'discovery of Arctic ozone hole',
-    imageId: 'mynHUSa',
-  },
-  {
-    id: 2,
-    name: 'Mohammad Abdus Salam',
-    profession: 'physicist',
-    accomplishment: 'electromagnetism theory',
-    imageId: 'bE7W1ji',
-  },
-  {
-    id: 3,
-    name: 'Percy Lavon Julian',
-    profession: 'chemist',
-    accomplishment:
-      'pioneering cortisone drugs, steroids and birth control pills',
-    imageId: 'IOjWm71',
-  },
-  {
-    id: 4,
-    name: 'Subrahmanyan Chandrasekhar',
-    profession: 'astrophysicist',
-    accomplishment: 'white dwarf star mass calculations',
-    imageId: 'lrWQx8l',
-  },
+const todos = [
+  { task: 'mow the yard', id: uuid() },
+  { task: 'Work on Odin Projects', id: uuid() },
+  { task: 'feed the cat', id: uuid() },
 ];
 
-function getImageUrl(person) {
-  return 'https://i.imgur.com/' + person.imageId + 's.jpg';
-}
-
 function App() {
-  const listItems = people.map((person) => (
-    <li key={person.id}>
-      <img src={getImageUrl(person)} alt={person.name} />
-      <p>
-        <b>{person.name}:</b>
-        {' ' + person.profession + ' '}
-        known for {person.accomplishment}
-      </p>
-    </li>
-  ));
-  return <ul>{listItems}</ul>;
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.task}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
