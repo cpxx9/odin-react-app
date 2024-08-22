0b26300f6345b89761e7723a2cb3031f1aee8f5d
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-08-22 07:05:45 -0400

    state updater functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,20 @@ import './App.css';
 // import { v4 as uuid } from 'uuid';
 import { useState } from 'react';
 
-const COLORS = ['pink', 'green', 'blue', 'yellow', 'purple'];
-
 function App() {
-  const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
-  const [timesChanged, setTimesChanged] = useState(0);
+  const [person, setPerson] = useState({ name: 'John', age: 100 });
 
-  const onButtonClick = (color) => () => {
-    setBackgroundColor(color);
-    setTimesChanged(timesChanged + 1);
+  const handleIncreaseAge = () => {
+    setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1 }));
+    setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1 }));
   };
 
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor,
-      }}
-    >
-      <h2>Times Changed: {timesChanged}</h2>
-      {COLORS.map((color) => (
-        <button
-          type="button"
-          key={color}
-          onClick={onButtonClick(color)}
-          className={backgroundColor === color ? 'selected' : ''}
-        >
-          {color}
-        </button>
-      ))}
-    </div>
+    <>
+      <h1>{person.name}</h1>
+      <h2>{person.age}</h2>
+      <button onClick={handleIncreaseAge}>Increase Age</button>
+    </>
   );
 }
 
