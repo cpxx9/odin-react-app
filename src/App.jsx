e7d6aba4581627e18e1b509fcb9d438a21a791ff
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-08-22 08:16:48 -0400

    react redundant state

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,65 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [person, setPerson] = useState({ name: 'John', age: 100 });
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [fullName, setFullName] = useState('');
 
-  const handleIncreaseAge = () => {
-    setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1 }));
-    setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1 }));
-  };
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
+    setFullName(e.target.value + ' ' + lastName);
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+    setFullName(firstName + ' ' + e.target.value);
+  }
 
   return (
     <>
-      <h1>{person.name}</h1>
-      <h2>{person.age}</h2>
-      <button onClick={handleIncreaseAge}>Increase Age</button>
+      <h2>Let&apos;s check you in</h2>
+      <label>
+        First Name:
+        <input value={firstName} onChange={handleFirstNameChange} />
+      </label>
+      <label>
+        Last Name:
+        <input value={lastName} onChange={handleLastNameChange} />
+      </label>
+      <p>
+        Your ticket will be issues to: <b>{fullName}</b>
+      </p>
     </>
   );
 }
+
+// function CustomInput() {
+//   const [value, setValue] = useState('');
+
+//   return (
+//     <input
+//       type="text"
+//       value={value}
+//       onChange={(event) => setValue(event.target.value)}
+//     />
+//   )
+// }
+
+// function App() {
+//   const [person, setPerson] = useState({ name: 'John', age: 100 });
+
+//   const handleIncreaseAge = () => {
+//     setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1 }));
+//     setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1 }));
+//   };
+
+//   return (
+//     <>
+//       <h1>{person.name}</h1>
+//       <h2>{person.age}</h2>
+//       <button onClick={handleIncreaseAge}>Increase Age</button>
+//     </>
+//   );
+// }
 
 export default App;
