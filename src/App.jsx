95c90f589c81599b0ee406ad543842ad1a9ebda9
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-08-25 13:32:18 -0400

    finished first react lessons

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,65 @@
 import './App.css';
 // import { v4 as uuid } from 'uuid';
 import { useState } from 'react';
-import { initialTravelPlan } from './data';
 
-function PlaceTree({ id, parentId, placesById, onComplete }) {
-  const place = placesById[id];
-  const childIds = place.childIds;
+function CustomInput({ placeholder, onChange, value }) {
+  // const [value, setValue] = useState('');
+
   return (
-    <li>
-      {place.title}
-      <button onClick={() => onComplete(parentId, id)}>Complete</button>
-      {childIds.length > 0 && (
-        <ol>
-          {childIds.map((childId) => (
-            <PlaceTree
-              key={childId}
-              id={childId}
-              parentId={id}
-              placesById={placesById}
-              onComplete={onComplete}
-            />
-          ))}
-        </ol>
-      )}
-    </li>
+    <input
+      type="text"
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
   );
 }
 
 function App() {
-  const [plan, setPlan] = useState(initialTravelPlan);
+  const [person, setPerson] = useState({
+    firstName: 'John',
+    lastName: 'Ham',
+    age: 100,
+  });
 
-  function handleComplete(parentId, childId) {
-    const parent = plan[parentId];
-    const nextParent = {
-      ...parent,
-      childIds: parent.childIds.filter((id) => id !== childId),
-    };
-    setPlan({
-      ...plan,
-      [parentId]: nextParent,
-    });
+  const handleIncreaseAge = () => {
+    setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1 }));
+    setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1 }));
+  };
+
+  function handleFirstNameChange(e) {
+    setPerson((prevPerson) => ({
+      ...prevPerson,
+      firstName: e.target.value,
+    }));
   }
 
-  const root = plan[0];
-  const planetIds = root.childIds;
+  function handleLastNameChange(e) {
+    setPerson((prevPerson) => ({
+      ...prevPerson,
+      lastName: e.target.value,
+    }));
+  }
 
   return (
     <>
-      <h2>Places to visit</h2>
-      <ol>
-        {planetIds.map((id) => (
-          <PlaceTree
-            key={id}
-            id={id}
-            parentId={0}
-            placesById={plan}
-            onComplete={handleComplete}
-          />
-        ))}
-      </ol>
+      <CustomInput
+        placeholder={'First name'}
+        onChange={handleFirstNameChange}
+        value={person.firstName}
+      />
+      <CustomInput
+        placeholder={'Last name'}
+        onChange={handleLastNameChange}
+        value={person.lastName}
+      />
+      <h1>
+        {person.firstName} {person.lastName}
+      </h1>
+      <h2>{person.age}</h2>
+      <button onClick={handleIncreaseAge}>Increase Age</button>
     </>
   );
 }
-
-// function CustomInput() {
-//   const [value, setValue] = useState('');
-
-//   return (
-//     <input
-//       type="text"
-//       value={value}
-//       onChange={(event) => setValue(event.target.value)}
-//     />
-//   )
-// }
-
-// function App() {
-//   const [person, setPerson] = useState({ name: 'John', age: 100 });
-
-//   const handleIncreaseAge = () => {
-//     setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1 }));
-//     setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1 }));
-//   };
-
-//   return (
-//     <>
-//       <h1>{person.name}</h1>
-//       <h2>{person.age}</h2>
-//       <button onClick={handleIncreaseAge}>Increase Age</button>
-//     </>
-//   );
-// }
 
 export default App;
