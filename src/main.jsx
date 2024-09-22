f3b50841f08a43b13e7cf66c3e4d8fb21957e09e
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-09-22 09:43:44 -0400

    routes separate file now

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,10 @@
-import App from './App.jsx';
-import Profile from './components/Profile.jsx';
 import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import routes from './routes';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: 'profile',
-    element: <Profile />,
-  },
-]);
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
