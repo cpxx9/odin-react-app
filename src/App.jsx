9abd03a545fb09997b0cfb78da76f819c761b000
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-09-22 09:11:55 -0400

    basic react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
 import './App.css';
+import { Link } from 'react-router-dom';
 
-function App(props) {}
+function App() {
+  return (
+    <div>
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are some examples of links to other pages</p>
+      <nav>
+        <ul>
+          <li>
+            <Link to="profile">Profile apge</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import Profile from './components/Profile.jsx';
 import './index.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: 'profile',
+    element: <Profile />,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
 );
