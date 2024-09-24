d764978bde056aece37a854c725f675d37b7b276
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-09-24 07:40:41 -0400

    odin react router and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,70 @@
 import './App.css';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
-function App() {
+export const issueUrl =
+  'https://api-sage-two-60.vercel.app/mocks/issues/1?delay=2000';
+
+const sidebarUrl =
+  'https://api-sage-two-60.vercel.app/mocks/sidebar?delay=1000';
+
+const commentsUrl =
+  'https://api-sage-two-60.vercel.app/mocks/comments?delay=3000';
+
+const useData = (url) => {
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    const dataFetch = async () => {
+      const data = await (await fetch(url)).json();
+      setState(data);
+    };
+    dataFetch();
+  }, [url]);
+  return { data: state };
+};
+
+const Sidebar = () => {
+  return;
+};
+
+const Comments = () => {
+  const { data } = useData(commentsUrl);
+
+  // show loading state while waiting for the data
+  if (!data) return 'loading';
+
+  // rendering comments now that we have access to them!
+  return data.map((comment) => <div>{comment.title}</div>);
+};
+
+const Issue = () => {
+  const { data } = useData(issueUrl);
+
+  // show loading state while waiting for the data
+  if (!data) return 'loading';
+
+  // render actual issue now that the data is here!
   return (
     <div>
-      <h1>Hello from the main page of the app!</h1>
-      <p>Here are some examples of links to other pages</p>
-      <nav>
-        <ul>
-          <li>
-            <Link to="profile">Profile apge</Link>
-          </li>
-        </ul>
-      </nav>
+      <h3>{data.title}</h3>
+      <p>{data.description}</p>
+      <Comments />
     </div>
+  );
+};
+
+function App() {
+  const { data } = useData(sidebarUrl);
+
+  // show loading state while waiting for the data
+  if (!data) return 'loading';
+
+  return (
+    <>
+      <Sidebar data={data} />
+      <Issue />
+    </>
   );
 }
 
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,27 @@
 import App from './App';
 import Profile from './components/Profile';
 import ErrorPage from './components/ErrorPage';
+import DefaultProfile from './components/DefaultProfile';
+import { useEffect } from 'react';
+
+const useData = (url) => {
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    const dataFetch = async () => {
+      const data = await (await fetch(url)).json();
+      setState(data);
+    };
+    dataFetch();
+  }, [url]);
+  return { data: state };
+};
 
 const routes = [
   {
     path: '/',
     element: <App />,
-    errorElement: <ErrorPage />,
+    errorElement: <DefaultProfile />,
   },
   {
     path: 'profile/:name',
